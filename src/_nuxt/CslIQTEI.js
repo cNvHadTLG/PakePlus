0bac51a222782cbe176bb5d37f6e5971267629b2
cNvHadTLG <mmamammamamambhy@gmail.com>
2025-06-29 20:52:55 +0800

    update src file from pakeplus

---
+++ src/_nuxt/CslIQTEI.js
@@ -0,0 +1,9 @@
+import{d as y,c as g,a as l,b as d,u as p,e as v}from"./CszauIMC.js";import{w as b,u as C}from"./Cca8yeN8.js";import{f as _}from"./XpcH3hjX.js";import{d as $,c as m,W as z,e as a}from"./CtvzWlFT.js";function D(e){const{textColorBase:n,opacity1:i,opacity2:s,opacity3:r,opacity4:o,opacity5:t}=e;return{color:n,opacity1Depth:i,opacity2Depth:s,opacity3Depth:r,opacity4Depth:o,opacity5Depth:t}}const x={common:y,self:D},R=g("icon",`
+ height: 1em;
+ width: 1em;
+ line-height: 1em;
+ text-align: center;
+ display: inline-block;
+ position: relative;
+ fill: currentColor;
+`,[l("color-transition",{transition:"color .3s var(--n-bezier)"}),l("depth",{color:"var(--n-color)"},[d("svg",{opacity:"var(--n-opacity)",transition:"opacity .3s var(--n-bezier)"})]),d("svg",{height:"1em",width:"1em"})]),S=Object.assign(Object.assign({},p.props),{depth:[String,Number],size:[Number,String],color:String,component:[Object,Function]}),O=$({_n_icon__:!0,name:"Icon",inheritAttrs:!1,props:S,setup(e){const{mergedClsPrefixRef:n,inlineThemeDisabled:i}=C(e),s=p("Icon","-icon",R,x,e,n),r=a(()=>{const{depth:t}=e,{common:{cubicBezierEaseInOut:c},self:h}=s.value;if(t!==void 0){const{color:u,[`opacity${t}Depth`]:f}=h;return{"--n-bezier":c,"--n-color":u,"--n-opacity":f}}return{"--n-bezier":c,"--n-color":"","--n-opacity":""}}),o=i?v("icon",a(()=>`${e.depth||"d"}`),r,e):void 0;return{mergedClsPrefix:n,mergedStyle:a(()=>{const{size:t,color:c}=e;return{fontSize:_(t),color:c}}),cssVars:i?void 0:r,themeClass:o==null?void 0:o.themeClass,onRender:o==null?void 0:o.onRender}},render(){var e;const{$parent:n,depth:i,mergedClsPrefix:s,component:r,onRender:o,themeClass:t}=this;return!((e=n==null?void 0:n.$options)===null||e===void 0)&&e._n_icon__&&b("icon","don't wrap `n-icon` inside `n-icon`"),o==null||o(),m("i",z(this.$attrs,{role:"img",class:[`${s}-icon`,t,{[`${s}-icon--depth`]:i,[`${s}-icon--color-transition`]:i!==void 0}],style:[this.cssVars,this.mergedStyle]}),r?m(r):this.$slots)}});export{O as N};
